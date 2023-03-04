a6fe4b746f754af846443acf222873256476e9c5
PotapenkoVadim <potapenko.v00@mail.ru>
2023-03-04 19:11:30 +0300

    feat: implement not found page

--- src/pages/not-found/index.jsx
+++ src/pages/not-found/index.jsx
@@ -1,7 +1,21 @@
+import { NavLink } from 'react-router-dom';
+import Button from '../../components/ui-kit/button';
+import { configuration } from '../../configuration';
+import styles from './not-found.module.scss';
+
+import deathStarImage from '../../assets/images/death-star.png';
+
+const routes = configuration.routes;
+
 export default function NotFoundPage() {
   return (
-    <main>
-      <h1>Error 404</h1>
+    <main className={styles['notfound']}>
+      <div className={styles['notfound__code']}>404</div>
+      <img className={styles['notfound__poster']} src={deathStarImage} alt='Death Star' />
+
+      <Button color='green'>
+        <NavLink className={styles['notfound__button-link']} to={routes.home}>Return</NavLink>
+      </Button>
     </main>
   );
 }
